ccac1513b01212cfda0096a7938b56b9256ae291
mehedihasanmondol <mehedihasanmondol.online@gmail.com>
2025-01-31 13:02:01 +0600

    single page title with feature image if available

--- page.php
+++ page.php
@@ -1,7 +1,24 @@
 <?php get_header(); ?>
+
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
+        <!-- PAGE TITLE SECTION -->
+        <?php
+        if (has_post_thumbnail()) {
+            $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+        ?>
+            <section class="clearfix pageTitleArea" style="background-image: url(<?php echo $thumbnail_url ?>);">
+                <div class="container">
+                    <div class="pageTitle">
+                        <h1>
+
+                            <?php the_title(); ?>
+                        </h1>
+                    </div>
+                </div>
+            </section>
+        <?php } ?>
+        <?php the_content(); ?>
 <?php endwhile;
 endif; ?>
 
