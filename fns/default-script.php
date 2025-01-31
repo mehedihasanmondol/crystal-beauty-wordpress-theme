f8e8903b86707d425f70908dde345e74e82649ed
mehedihasanmondol <mehedihasanmondol.online@gmail.com>
2025-02-01 03:50:04 +0600

    google font load issue fixed

--- fns/default-script.php
+++ fns/default-script.php
@@ -1,7 +1,8 @@
 <?php
 function crystalbeauty_enqueue_scripts()
 {
-    wp_enqueue_style('google_font', 'https://fonts.googleapis.com/css2?family=Herr+Von+Muellerhoff&family=Montserrat:wght@400;700&family=Open+Sans:wght@300;400;600;700&display=swap');
+    wp_enqueue_style('google_font', esc_url_raw('https://fonts.googleapis.com/css2?family=Herr+Von+Muellerhoff&family=Montserrat:wght@400;700&family=Open+Sans:wght@300;400;600;700&display=swap'), array(), null);
+
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/plugins/bootstrap/css/bootstrap.min.css');
     wp_enqueue_style('fontawesome', get_template_directory_uri() . '/plugins/font-awesome/css/font-awesome.min.css');
     wp_enqueue_style('animate', get_template_directory_uri() . '/plugins/animate/animate.css');
